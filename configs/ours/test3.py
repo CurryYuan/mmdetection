30969db65f0262fe95307c226776fdebd6586e27
CurryYuan <1172248391@qq.com>
2019-08-16 19:02:00 +0800

    Update test3.py

--- configs/ours/test3.py
+++ configs/ours/test3.py
@@ -2,14 +2,16 @@
 model = dict(
     type='MyDetector',
     num_stages=2,
-    pretrained='modelzoo://resnet50',
+    pretrained='open-mmlab://resnet50_caffe',
     backbone=dict(
         type='ResNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        style='pytorch'),
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='caffe'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -194,7 +196,7 @@ total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/CascadeRPN'
-load_from = './weights/faster_rcnn_r50_fpn_1x_20181010-3d1b3351.pth'
+load_from = './weights/ga_faster_r50_caffe_fpn_1x_20190513-a52b31fa.pth'
 resume_from = None
 workflow = [('train', 1)]
 
