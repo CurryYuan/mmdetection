e4f5bbdc2427ceff91277d3c378663ad13101efe
CurryYuan <1172248391@qq.com>
2019-08-16 15:54:01 +0800

    Update test3.py

--- configs/ours/test3.py
+++ configs/ours/test3.py
@@ -179,7 +179,7 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[9, 12])
+    step=[8, 11])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
@@ -190,7 +190,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 13
+total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/CascadeRPN'
